6456f1f9dd92a763e7c3fa4babacd4b1cae8f5b9
Tom Bayon <bayon.tom.01@gmail.com>
2025-11-17 21:00:27 +0100

    1er commit mdp

---
+++ est_mot_de_passe_valide.py
@@ -0,0 +1,23 @@
+def est_mot_de_passe_valide(mot):
+    
+    longueur_ok = len(mot) >= 8
+    chiffre_ok = False
+    maj_ok = False
+
+    for c in mot:
+        
+        if c >= "0" and c <= "9":
+            chiffre_ok = True
+
+        
+        if c >= "A" and c <= "Z":
+            maj_ok = True
+
+   
+    return longueur_ok and chiffre_ok and maj_ok
+
+
+
+print(est_mot_de_passe_valide("Ab123456"))  
+print(est_mot_de_passe_valide("ab123456"))  
+print(est_mot_de_passe_valide("ABCDEFGH"))  
