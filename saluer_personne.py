d7b88c41cb16e196841b7425c346ccdc0c91de6c
Tom Bayon <bayon.tom.01@gmail.com>
2025-11-17 17:02:48 +0100

    1er commit

---
+++ saluer_personne.py
@@ -0,0 +1,16 @@
+def saluer_personne(nom, heure):
+    if 6 <= heure < 12:
+        return f"Bonjour {nom}"
+    elif 12 <= heure < 18:
+        return f"Bon après-midi {nom}"
+    elif 18 <= heure < 24:
+        return f"Bonsoir {nom}"
+    else:
+        return f"Bonne nuit {nom}"
+
+
+
+print(saluer_personne("Tom", 24))   
+print(saluer_personne("Victor", 14))  
+print(saluer_personne("Marie", 20))    
+print(saluer_personne("Paul", 3))     
