a1489de24fc279ef9f0166256a6929962b58b77f
Tom Bayon <bayon.tom.01@gmail.com>
2025-11-18 10:08:54 +0100

    3eme commit calculer prix

--- calculer_prix_ttc.py
+++ calculer_prix_ttc.py
@@ -1,5 +1,6 @@
 def calculer_prix_ttc(prix_ht, tva=20):
-    return float(round(prix_ht *(1 + tva/100),2))
+        """calculer le prix avec la tva """
+        return float(round(prix_ht *(1 + tva/100),2))
 
 
-print(calculer_prix_ttc(55))
+print("Le prix avec la TVA est :", calculer_prix_ttc(55),"euros")
